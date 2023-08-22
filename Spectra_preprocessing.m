cb6346fa67456febdcf06076b2930ecbb9c15a84
d-robles <drobles@ualberta.ca>
2023-08-22 15:01:55 -0400

    testing

--- Spectra_preprocessing.m
+++ Spectra_preprocessing.m
@@ -54,11 +54,13 @@
 
 ccc
 parts = {'0017'};
-session = {'03'};
-tmstarget = {'03'};
-conditions = {'positive';'negative'};
+session = {'S03'};
+tmstarget = {'T03'};
+%conditions = {'positive';'negative'};
 filepath = ['C:\Users\Kathryn\Documents\PST_DR\raw_files'];% C:\Users\Kathryn\Documents\PST_DR\
 exp = 'PST';
+i_sess = 1:length(session); 
+i_targ = 1:length(tmstarget);
 
 %%%old settings for selection cards%%%
 % % selection_cards_far = {'1 2 3 4', '5', '21 22 23 24 25'};
@@ -88,73 +90,75 @@ eeg_thresh_2 = [-500,500];
 %%
 for i_part = 1:length(parts)
     
-    for i_cond = 1:length(conditions)
-        
-        
-        events = {'1' '2' '3' '4'};
-%         selection_cards = {'1' '2'};
-        selection_cards = {'1' '2'};
-
-        %%%load data%%%
-        disp(['Processing data for participant ' parts{i_part}]);
-        disp(['Loading file: ' parts{i_part} '_' exp '_' conditions{i_cond} '.vhdr']);
-%         filename = [parts{i_part} '_' exp '_' conditions{i_cond} '.vhdr'];
-%         EEG = pop_loadbv(filepath, filename, [], []); % loading in EEG data
-%         savename = [parts{i_part} '_' exp '_' conditions{i_cond}];
-        %exp-sessopm-tmstarg-part
-        filename = [exp  conditions{i_cond}parts{i_part}  '.vhdr'];
-        EEG = pop_loadbv(filepath, filename, [], []); % loading in EEG data
-        savename = [parts{i_part} '_' exp '_' conditions{i_cond}];
-        % get electrode locations
-        EEG=pop_chanedit(EEG, 'load',{'C:\Users\Kathryn\Documents\PST_DR\stms.ced' 'filetype' 'autodetect'});
-        
-        % arithmetically rereference to linked mastoid
-        for x=1:EEG.nbchan-2
-            EEG.data(x,:) = (EEG.data(x,:)-((EEG.data(EEG.nbchan-2,:))*.5));
-        end
-        
-        %Filter the data with low pass of 30
-        EEG = pop_eegfilt( EEG, high_pass, 0, [], 0);  %high pass filter
-        EEG = pop_eegfilt( EEG, 0, low_pass, [], 0);  %low pass filter
-        
-        
-        all_events = length(EEG.event);
-        
-        
-        for i_event = 2:all_events %%% Why start from 2
-            if strcmp(EEG.event(i_event).type, 'boundary')
-                continue
-            else
-                EEG.event(i_event).type = num2str(str2num((EEG.event(i_event).type(2:end))));
+    for i_sess = 1:length(session)
+        for i_targ = 1:length (tmstarget)
+            
+            events = {'1' '2' '3' '4'};
+            %         selection_cards = {'1' '2'};
+            selection_cards = {'1' '2'};
+            
+            %%%load data%%%
+            disp(['Processing data for participant ' parts{i_part}]);
+            disp(['Loading file: ' exp session{i_sess} tmstarget{i_targ} parts{i_part} '.vhdr']);
+            %         filename = [parts{i_part} '_' exp '_' conditions{i_cond} '.vhdr'];
+            %         EEG = pop_loadbv(filepath, filename, [], []); % loading in EEG data
+            %         savename = [parts{i_part} '_' exp '_' conditions{i_cond}];
+            %exp-sessopm-tmstarg-part
+            filename = [exp session{i_sess} tmstarget{i_targ} '_' parts{i_part} '.vhdr'];
+            EEG = pop_loadbv(filepath, filename, [], []); % loading in EEG data
+            savename = [exp session{i_sess} tmstarget{i_targ} parts{i_part}];
+            % get electrode locations
+            EEG=pop_chanedit(EEG, 'load',{'C:\Users\Kathryn\Documents\PST_DR\stms.ced' 'filetype' 'autodetect'});
+            
+            % arithmetically rereference to linked mastoid
+            for x=1:EEG.nbchan-2
+                EEG.data(x,:) = (EEG.data(x,:)-((EEG.data(EEG.nbchan-2,:))*.5));
             end
+            
+            %Filter the data with low pass of 30
+            EEG = pop_eegfilt( EEG, high_pass, 0, [], 0);  %high pass filter
+            EEG = pop_eegfilt( EEG, 0, low_pass, [], 0);  %low pass filter
+            
+            
+            all_events = length(EEG.event);
+            
+            
+            for i_event = 2:all_events %%% Why start from 2
+                if strcmp(EEG.event(i_event).type, 'boundary')
+                    continue
+                else
+                    EEG.event(i_event).type = num2str(str2num((EEG.event(i_event).type(2:end))));
+                end
+            end
+            
+            %epoch
+            
+            EEG = pop_epoch( EEG, events, epoch_size, 'newname',  sprintf('%s epochs' , exp), 'epochinfo', 'yes'); %Changed from [-.2 1] to [-1 2]. DR
+            %S1 is standard, S2 = target....
+            EEG = pop_rmbase( EEG, baseline);
+            
+            %    Artifact rejection, trials with range >500 uV
+            EEG = pop_eegthresh(EEG,1,[1:size(EEG.data,1)],eeg_thresh_1(1),eeg_thresh_1(2),EEG.xmin,EEG.xmax,0,1);
+            
+            %   EMCP occular correction
+            temp_ocular = EEG.data(end-1:end,:,:); %to save the EYE data for after
+            EEG = gratton_emcp(EEG,selection_cards,{'VEOG'},{'HEOG'}); %this assumes the eye channels are called this
+            EEG.emcp.table %this prints out the regression coefficients
+            EEG.data(end-1:end,:,:) = temp_ocular; %replace the eye data
+            
+            %    Artifact rejection, trials with range >250 uV
+            EEG = pop_rmbase( EEG, baseline); %baseline again since this changed it
+            EEG = pop_eegthresh(EEG,1,[1:size(EEG.data,1)-2],eeg_thresh_2(1),eeg_thresh_2(2),EEG.xmin,EEG.xmax,0,1);
+            
+            EEG_Copy = EEG;
+            
+            EEG = pop_selectevent(EEG_Copy, 'type',str2num(events{1}),'renametype','Targets','deleteevents','on','deleteepochs','on','invertepochs','off');
+            EEG = pop_editset(EEG, 'setname',[savename '_fft_Targets']);
+            EEG = pop_saveset(EEG, 'filename',[savename '_fft_Targets'],'filepath',[filepath 'segments_fft_JK\']);
+            
         end
-        
-        %epoch
-        
-        EEG = pop_epoch( EEG, events, epoch_size, 'newname',  sprintf('%s epochs' , exp), 'epochinfo', 'yes'); %Changed from [-.2 1] to [-1 2]. DR
-        %S1 is standard, S2 = target....
-        EEG = pop_rmbase( EEG, baseline);
-        
-        %    Artifact rejection, trials with range >500 uV
-        EEG = pop_eegthresh(EEG,1,[1:size(EEG.data,1)],eeg_thresh_1(1),eeg_thresh_1(2),EEG.xmin,EEG.xmax,0,1);
-        
-        %   EMCP occular correction
-        temp_ocular = EEG.data(end-1:end,:,:); %to save the EYE data for after
-        EEG = gratton_emcp(EEG,selection_cards,{'VEOG'},{'HEOG'}); %this assumes the eye channels are called this
-        EEG.emcp.table %this prints out the regression coefficients
-        EEG.data(end-1:end,:,:) = temp_ocular; %replace the eye data
-        
-        %    Artifact rejection, trials with range >250 uV
-        EEG = pop_rmbase( EEG, baseline); %baseline again since this changed it
-        EEG = pop_eegthresh(EEG,1,[1:size(EEG.data,1)-2],eeg_thresh_2(1),eeg_thresh_2(2),EEG.xmin,EEG.xmax,0,1);
-        
-        EEG_Copy = EEG;
-        
-        EEG = pop_selectevent(EEG_Copy, 'type',str2num(events{1}),'renametype','Targets','deleteevents','on','deleteepochs','on','invertepochs','off');
-        EEG = pop_editset(EEG, 'setname',[savename '_fft_Targets']);
-        EEG = pop_saveset(EEG, 'filename',[savename '_fft_Targets'],'filepath',[filepath 'segments_fft_JK\']);
-        
     end
 end
+
 %%
 
